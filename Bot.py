bedc5b589ac50157d9bf40e4dda4b9a4210b5903
mir2255 <jamshidxusinov80@gmail.com>
2026-02-27 04:45:19 +0500

    Refactor start command message and markup

--- Bot.py
+++ Bot.py
@@ -1,17 +1,17 @@
 import telebot
 from telebot import types
 
-# @BotFather bergan API Tokenni shu yerga qo'ying
-TOKEN = '8152541467:AAH_u3WqS_0Q-kUu9LdG2Z5V_8U6V5U6V5U' # O'zingizning tokengingizni aniq yozing
+TOKEN = '8152541467:AAH_u3WqS_0Q-kUu9LdG2Z5V_8U6V5U6V5U'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    web_app = types.WebAppInfo("https://mir2255.github.io/My_stars_bot/")
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    # WEB APP URL manzilingizni tekshirib oling (oxiri / bilan tugashi kerak)
+    web_app = types.WebAppInfo("https://mir2255.github.io/My_stars_bot/") 
     game_button = types.KeyboardButton("⭐ O'yinni boshlash", web_app=web_app)
     markup.add(game_button)
     
-    bot.send_message(message.chat.id, f"Salom {message.from_user.first_name}! O'yinga xush kelibsiz!", reply_markup=markup)
+    bot.send_message(message.chat.id, f"Salom {message.from_user.first_name}! O'yinni boshlash uchun tugmani bosing:", reply_markup=markup)
 
 bot.polling(none_stop=True)
