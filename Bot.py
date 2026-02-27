f0c196116ab475b984a23bb0d3b7ef7d31398357
mir2255 <jamshidxusinov80@gmail.com>
2026-02-27 07:23:28 +0500

    Update welcome message in Bot.py

--- Bot.py
+++ Bot.py
@@ -1,16 +1,19 @@
 import telebot
 from telebot import types
 
-TOKEN = '8361228448:AAF1x3Y87Q0vmAEs_Dp9xnJNWGJdJYCyfsg' # Tokeningizni tekshiring
+# BotFather'dan olgan tokenni mana shu qo'shtirnoq ichiga aniq qilib qo'ying
+TOKEN = '8361228448:AAF1x3Y87Q0vmAEs_Dp9xnJNWGJdJYCyfsg' 
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
+    # O'yiningiz manzili
     web_app = types.WebAppInfo("https://mir2255.github.io/My_stars_bot/")
     btn = types.InlineKeyboardButton("🎮 O'yinni boshlash", web_app=web_app)
     markup.add(btn)
-    bot.send_message(message.chat.id, "Salom! O'yin tayyor. Boshlash uchun bosing:", reply_markup=markup)
+    
+    bot.send_message(message.chat.id, "Xush kelibsiz! O'yinni boshlash uchun tugmani bosing:", reply_markup=markup)
 
 bot.polling(none_stop=True)
