546819e733e7c4a8b6159837578a6fbc7cce3b38
mir2255 <jamshidxusinov80@gmail.com>
2026-02-27 07:09:36 +0500

    Update bot token and modify start message

--- Bot.py
+++ Bot.py
@@ -1,17 +1,16 @@
 import telebot
 from telebot import types
 
-TOKEN = '8361228448:AAF1x3Y87Q0vmAEs_Dp9xnJNWGJdJYCyfsg'
+TOKEN = '7629202302:AAHgO_WzL94XpW9L1kM_XqY_lXzXzXzXzXz' # Tokeningizni tekshiring
+
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
-    # Siz yaratgan o'yin linki
     web_app = types.WebAppInfo("https://mir2255.github.io/My_stars_bot/")
-    btn = types.InlineKeyboardButton("O'yinni ochish", web_app=web_app)
+    btn = types.InlineKeyboardButton("🎮 O'yinni boshlash", web_app=web_app)
     markup.add(btn)
-    
-    bot.send_message(message.chat.id, "Tabriklayman! Bot ishladi! ✅\nO'yinni boshlash uchun tugmani bosing:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Salom! O'yin tayyor. Boshlash uchun bosing:", reply_markup=markup)
 
 bot.polling(none_stop=True)
