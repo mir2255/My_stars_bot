5460c1385bc8dea15a48aac3bde58fc55cd9cec6
mir2255 <jamshidxusinov80@gmail.com>
2026-02-27 05:25:21 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,18 +1,23 @@
 import telebot
 from telebot import types
 
-TOKEN = '8361228448:AAENkqz-ksApxYloCpd1ZPPQDWz3-3_WbMw'
+# O'z tokiningizni kiriting
+TOKEN = '8361228448:AAENkqz-ksApxYloCpd1ZPPQDWA...' 
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(row_width=1)
-    # WEB APP URL manzilingizni tekshirib oling (oxiri / bilan tugashi kerak)
-    web_app = types.WebAppInfo("https://mir2255.github.io/My_stars_bot/") 
-    game_button = types.KeyboardButton("⭐ O'yinni boshlash", web_app=web_app)
-    markup.add(game_button)
+    # Tugmalar to'plami
+    markup = types.InlineKeyboardMarkup()
     
-    bot.send_message(message.chat.id, f"Salom {message.from_user.first_name}! O'yinni boshlash uchun tugmani bosing:", reply_markup=markup)
+    # BU YERGA O'YIN SAYTINGIZ MANZILINI QO'YING
+    web_app = types.WebAppInfo("https://sizning-saytingiz.uz") 
+    
+    button = types.InlineKeyboardButton("O'yinni ochish", web_app=web_app)
+    markup.add(button)
+    
+    bot.send_message(message.chat.id, "Salom! O'yinni boshlash uchun pastdagi tugmani bosing:", reply_markup=markup)
 
+# BOTNI TIRSILATIB ISHLATADIGAN ASOSIY QATOR:
 bot.polling(none_stop=True)
